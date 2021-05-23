ad9444d2368254a450a7162c5a74e3d04ed375c0
Sigalsha <sigal.shalit.manor@gmail.com>
2021-05-23 15:54:23 +0300

    initial commit, build folder structure with css modules

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
-import About from './components/About'
-import Form from './components/Form'
-import {useForm} from 'react-hook-form';
-
+import { useForm } from "react-hook-form";
+import logo from "./assets/logo.jpeg";
+import Header from "./components/Header/index";
+import Footer from "./components/Footer/index";
+import About from "./components/About/index";
+import Form from "./components/Form/index";
 
 const App = () => {
-
   return (
-      <div className='container'>
-        <Header/>
-        <About />
-        <Footer />
-        <Form />
-      </div>
-  )
-}
+    <div className="container">
+      <img src={logo} alt="logo" className="logo"></img>
+      <Header />
+      <About />
+      <Footer />
+      <Form />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import styles from "./style.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>My Freedom</h1>
+    </header>
+  );
+};
+
+export default Header;
