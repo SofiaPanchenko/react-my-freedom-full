17c0782613b925614a3d6d3b4e10e5ad9e4b0f42
Sigalsha <sigal.shalit.manor@gmail.com>
2021-05-24 16:34:23 +0300

    add text lines to constants, initial conditional form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,4 +1,11 @@
+import { useState, ReactFragment } from "react";
 import { useForm } from "react-hook-form";
+import {
+  GENERAL,
+  SELF_REPORT,
+  REPORT_OTHER,
+  ERRORS
+} from "../../utils/constants";
 import styles from "./style.css";
 
 const Form = () => {
@@ -7,76 +14,178 @@ const Form = () => {
     handleSubmit,
     formState: { errors }
   } = useForm();
+  const { isSelfReport, setReportMode } = useState(true);
   const onSubmit = (data) => console.log(data);
 
   return (
     <form className="form-control" onSubmit={handleSubmit(onSubmit)}>
-      <p className="form-header">
-        היי, מלא/י את הטופס, ככל שתפרט/י נוכל לדייק את המענה. אנחנו מתחייבים
-        לשמור על דיסקרטיות!
-      </p>
-      <label htmlFor="fullName">שם מלא</label>
-      <input
-        id="fullName"
-        aria-invalid={errors.firstName ? "true" : "false"}
-        {...register("fullName", { required: true })}
-      />
-      {errors.fullName && <span role="alert">This field is required</span>}
+      <div>
+        <p className="form-header">
+          היי, מלא/י את הטופס, ככל שתפרט/י נוכל לדייק את המענה. אנחנו מתחייבים
+          לשמור על דיסקרטיות!
+        </p>
+      </div>
 
-      <label htmlFor="age">גיל</label>
-      <input
-        id="age"
-        aria-invalid={errors.age ? "true" : "false"}
-        {...register("age", { required: true })}
-      />
-      {errors.age && <span role="alert">This field is required</span>}
+      <br />
 
-      <label htmlFor="phoneNumber">טלפון</label>
-      <input
-        id="phoneNumber"
-        aria-invalid={errors.phoneNumber ? "true" : "false"}
-        {...register("phoneNumber", { required: true })}
-      />
-      {errors.phoneNumber && <span role="alert">This field is required</span>}
+      <div>
+        <label htmlFor="reporter">
+          <input
+            id="selfReporter"
+            type="radio"
+            name="reporter"
+            value={SELF_REPORT.reporterID}
+            className="radio"
+            {...register("reporter", { required: true })}
+            defaultChecked={isSelfReport === true}
+          />
+          <label htmlFor="selfReporter" className="sameLineLabel">
+            {SELF_REPORT.reporterID}
+          </label>
+          <br />
+          <input
+            id="reportForOther"
+            type="radio"
+            name="reporter"
+            value={REPORT_OTHER.reporterID}
+            className="radio"
+            {...register("reporter", { required: true })}
+            defaultChecked={isSelfReport === false}
+          />
+          <label htmlFor="selfReporter" id="radioLabel">
+            {REPORT_OTHER.reporterID}
+          </label>
+        </label>
+      </div>
 
-      <label htmlFor="adress">כתובת</label>
-      <input
-        id="adress"
-        aria-invalid={errors.adress ? "true" : "false"}
-        {...register("adress", { required: true })}
-      />
-      {errors.adress && <span role="alert">This field is required</span>}
+      {!isSelfReport && (
+        <div>
+          <label htmlFor="victimName">{REPORT_OTHER.victimID}</label>
+          <input
+            id="victimName"
+            aria-invalid={errors.victimName ? "true" : "false"}
+            {...register("victimName", { required: true })}
+          />
+          {errors.victimName && <span role="alert">{ERRORS.required}</span>}
+        </div>
+      )}
 
-      <label htmlFor="sector">מגזר</label>
-      <select
-        id="sector"
-        aria-invalid={errors.sector ? "true" : "false"}
-        {...register("sector", { required: true })}
-      >
-        <option value="Christian" selected>
-          ללא סיווג דת
-        </option>
-        <option value="Jewish">יהודי</option>
-        <option value="Muslim">מוסלמי</option>
-        <option value="Druze">דרוזי</option>
-        <option value="Christian">נוצרי</option>
-      </select>
-      {errors.sector && <span role="alert">This field is required</span>}
+      {isSelfReport ? (
+        <div>
+          <label htmlFor="fullName">שם מלא</label>
+          <input
+            id="fullName"
+            aria-invalid={errors.firstName ? "true" : "false"}
+            {...register("fullName", { required: true })}
+          />
+          {errors.fullName && <span role="alert">{ERRORS.required}</span>}
+        </div>
+      ) : (
+        <div>
+          <label htmlFor="reporterInfoTitle">{REPORT_OTHER.reporterInfo}</label>
+          <label htmlFor="fullName">{REPORT_OTHER.fullName}</label>
+          <input
+            id="fullName"
+            aria-invalid={errors.fullName ? "true" : "false"}
+            {...register("fullName", { required: true })}
+          />
+          {errors.fullName && <span role="alert">{ERRORS.required}</span>}
+        </div>
+      )}
 
-      <label htmlFor="gender">מגדר</label>
-      <select
-        id="gender"
-        value="לא מוגדר"
-        aria-invalid={errors.gender ? "true" : "false"}
-        {...register("gender", { required: true })}
-      >
-        <option value="undefined" selected>
-          לא מוגדר
-        </option>
-        <option value="Male">זכר</option>
-        <option value="Female">נקבה</option>
-      </select>
-      {errors.gender && <span role="alert">This field is required</span>}
+      <div>
+        <label htmlFor="age">{GENERAL.age}</label>
+        <input
+          id="age"
+          aria-invalid={errors.age ? "true" : "false"}
+          {...register("age", { required: true })}
+        />
+        {errors.age && <span role="alert">{ERRORS.required}</span>}
+      </div>
+
+      <div>
+        <label htmlFor="phoneNumber">{GENERAL.phoneNumber}</label>
+        <input
+          id="phoneNumber"
+          type="tel"
+          aria-invalid={errors.phoneNumber ? "true" : "false"}
+          {...register("phoneNumber", { required: true })}
+        />
+        {errors.phoneNumber && <span role="alert">{ERRORS.required}</span>}
+      </div>
+
+      <div>
+        <label htmlFor="address">{GENERAL.address}</label>
+        <input
+          id="address"
+          aria-invalid={errors.address ? "true" : "false"}
+          {...register("address", { required: true })}
+        />
+        {errors.address && <span role="alert">{ERRORS.required}</span>}
+      </div>
+
+      <div>
+        <label htmlFor="sector">{GENERAL.sector.sector}</label>
+        <select
+          id="sector"
+          aria-invalid={errors.sector ? "true" : "false"}
+          {...register("sector", { required: true })}
+        >
+          <option value="Christian" selected>
+            {GENERAL.sector.noReligion}
+          </option>
+          <option value="Jewish">{GENERAL.sector.jewish}</option>
+          <option value="Muslim">{GENERAL.sector.muslim}</option>
+          <option value="Druze">{GENERAL.sector.druze}</option>
+          <option value="Christian">{GENERAL.sector.christian}</option>
+        </select>
+        {errors.sector && <span role="alert">{ERRORS.required}</span>}
+      </div>
+
+      <div>
+        <label htmlFor="gender">{GENERAL.gender.gender}</label>
+        <select
+          id="gender"
+          value={GENERAL.gender.undefined}
+          aria-invalid={errors.gender ? "true" : "false"}
+          {...register("gender", { required: true })}
+        >
+          <option value="undefined" selected>
+            {GENERAL.gender.undefined}
+          </option>
+          <option value="Male">{GENERAL.gender.male}</option>
+          <option value="Female">{GENERAL.gender.female}</option>
+        </select>
+        {errors.gender && <span role="alert">{ERRORS.required}</span>}
+      </div>
+
+      {isSelfReport ? (
+        <div>
+          <input
+            type="checkbox"
+            {...register("closedCommunity", { required: true })}
+          />
+          <label htmlFor="closedCommunity" className="sameLineLabel">
+            {SELF_REPORT.closedCommunity}
+          </label>
+          {errors.closedCommunity && (
+            <span role="alert">{ERRORS.required}</span>
+          )}
+        </div>
+      ) : (
+        <div>
+          <input
+            type="checkbox"
+            {...register("closedCommunity", { required: true })}
+          />
+          <label htmlFor="closedCommunity" className="sameLineLabel">
+            {REPORT_OTHER.closedCommunity}
+          </label>
+          {errors.closedCommunity && (
+            <span role="alert">{ERRORS.required}</span>
+          )}
+        </div>
+      )}
 
       <input className="btn" type="submit" />
     </form>
@@ -84,3 +193,10 @@ const Form = () => {
 };
 
 export default Form;
+
+// checkbox
+
+/*    <div>
+        <label>Is developer?</label>
+        <input type="checkbox" {...register("isDeveloper")} />
+      </div> */
---
+++ src/utils/constants.js
@@ -0,0 +1,77 @@
+export const GENERAL = {
+  age: "גיל",
+  phoneNumber: "טלפון",
+  address: "כתובת",
+  sector: {
+    sector: "מגזר",
+    noReligion: "ללא סיווג דת",
+    jewish: "יהודי",
+    muslim: "מוסלמי",
+    druze: "דרוזי",
+    christian: "נוצרי"
+  },
+
+  gender: {
+    gender: "מגדר",
+    undefined: "לא מוגדר",
+    male: "זכר",
+    female: "נקבה"
+  },
+  violence: {
+    rateInfo: "אנא דרג/י מ-0 (ללא) עד 5 (קשה)",
+    physical: "אלימות פיזית",
+    sexual: "אלימות מינית",
+    mental: "אלימות נפשית",
+    economical: "אלימות כלכלית",
+    neglect: "הזנחה",
+    preventPsychoTreatment: "מניעת טיפול פסיכולוגי",
+    preventMedicalTreatment: "מניעת טיפול רפואי",
+    lifeThreat: "איום על חייך או חיי אחרים",
+    socialDisconnection: "ניתוק חברתי"
+  },
+  violenceDuration: "?משך זמן האלימות",
+  attacker: "מי הפוגע/ים?",
+  aidType: "איזה סיוע היית רוצה לקבל ?",
+  legalProcess: "האם קיים/ היה קיים הליך משפטי/פלילי מול הפוגע?",
+  contactInfo: "איפה ואיך ניתן ליצור איתך קשר לאחר שליחת הטופס?",
+  additionalComments: "הערות נוספות - האם יש משהו שחשוב שנדע?",
+  submit: "שליחה"
+};
+
+export const SELF_REPORT = {
+  reporterID: "אני סובל/ת מאלימות",
+  fullName: "שם מלא",
+  closedCommunity: "אני חלק מקהילה סגורה",
+  specialNeeds: "אני בעל/ת צרכים מיוחדים",
+  violence: {
+    violenceType: "",
+    rateInfo: "",
+    physical: "אלימות פיזית",
+    sexual: "אלימות מינית",
+    mental: "אלימות נפשית",
+    economical: "אלימות כלכלית",
+    neglect: "הזנחה",
+    preventPsychoTreatment: "מניעת טיפול פסיכולוגי",
+    preventMedicalTreatment: "מניעת טיפול רפואי",
+    lifeThreat: "איום על חייך או חיי אחרים",
+    socialDisconnection: "ניתוק חברתי"
+  },
+
+  tenantAttacker: "האם הפוגע גר איתך באותו הבית?",
+  injuryTreatment: "האם קיבלת טיפול רגשי/פיזי סביב הפגיעה?"
+};
+
+export const REPORT_OTHER = {
+  reporterID: "אני פונה עבור מישהו אחר",
+  victimID: "במידה ואת/ה פונה עבור מישהו אחר, על מי מדובר?",
+  reporterInfo: "פרטי ממלא/ת הטופס:",
+  fullName: "שם מלא של ממלא/ת הטופס",
+  closedCommunity: "הוא/היא חלק מקהילה סגורה",
+  specialNeeds: "הוא/היא בעל/ת צרכים מיוחדים",
+  tenantAttacker: "האם הפוגע גר איתו/איתה באותו הבית?",
+  injuryTreatment: "האם הוא/היא קיבל/ה טיפול רגשי/פיזי סביב הפגיעה?"
+};
+
+export const ERRORS = {
+  required: "שדה זה הוא חובה"
+};
